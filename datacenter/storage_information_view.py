8c12844024b9da37cdcafdafd90abd45797eb328
Aleksan-dres <galeks382@gmail.com>
2025-09-13 22:36:56 +0300

    Update storage_information_view.py
    Поправил названия переменных

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,29 +1,24 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-from .models import Visit 
+from .models import Visit
 
-
-    
 
 def storage_information_view(request):
-    at_the_entrance = Visit.objects.filter( leaved_at__isnull=True)
-    results = []
-    for visit in at_the_entrance: 
-        entered_original = visit.entered_at 
-        elapsed_time = visit.get_duration()  
-        
+    inside_the_storage = Visit.objects.filter(leaved_at__isnull=True)
+    inside = []
+    for visit in inside_the_storage:
+        went_inside = visit.entered_at
+
         formatted_elapsed_time = visit.format_duration()
-        
-        results.append({
+
+        inside.append({
             'who_entered': visit.passcard.owner_name,
-            'entered_at': entered_original , 
-            'duration': formatted_elapsed_time 
+            'entered_at': went_inside,
+            'duration': formatted_elapsed_time
         })
-         
+
     context = {
-        'non_closed_visits': results, 
+        'non_closed_visits': inside,
     }
     return render(request, 'storage_information.html', context)
-
 
