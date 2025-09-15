b86a90534afba834ee5c5c77afa311f2d82d4584
Aleksan-dres <galeks382@gmail.com>
2025-09-15 23:07:30 +0300

    Update passcard_info_view.py
    Добавил переменную для числа

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -4,21 +4,24 @@ from django.shortcuts import render, get_object_or_404
 from .models import Visit
 
 
+SECONDS_IN_MINUTE = 60
+
+
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
-    visits = Visit.objects.filter(passcard=passcard)   
+    visits = Visit.objects.filter(passcard=passcard)
     this_passcard_visits = []
-    for visit in visits: 
-        entered_at = visit.entered_at.strftime('%Y-%m-%d %H:%M:%S') 
+    for visit in visits:
+        entered_at = visit.entered_at.strftime('%Y-%m-%d %H:%M:%S')
         duration = visit.get_duration()
-        duration_in_minutes = int(duration.total_seconds() / 60)
+        duration_in_minutes = int(duration.total_seconds() / SECONDS_IN_MINUTE)
         is_strange = visit.is_visit_long()
         this_passcard_visits.append({
             'entered_at': entered_at,
             'duration': f'{duration_in_minutes} минут',
             'is_strange': is_strange
         })
- 
+
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
