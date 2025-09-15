14a43affb9771168a1ccb572b930fb7673191363
Aleksan-dres <galeks382@gmail.com>
2025-09-15 23:04:33 +0300

    Update storage_information_view.py
    Изменил названия переменных

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,21 +4,21 @@ from .models import Visit
 
 
 def storage_information_view(request):
-    inside_the_storage = Visit.objects.filter(leaved_at__isnull=True)
-    inside = []
-    for visit in inside_the_storage:
+    current_visits = Visit.objects.filter(leaved_at__isnull=True)
+    current_visitors = []
+    for visit in current_visits:
         went_inside = visit.entered_at
 
         formatted_elapsed_time = visit.format_duration()
 
-        inside.append({
+        current_visitors.append({
             'who_entered': visit.passcard.owner_name,
             'entered_at': went_inside,
             'duration': formatted_elapsed_time
         })
 
     context = {
-        'non_closed_visits': inside,
+        'non_closed_visits': current_visitors,
     }
     return render(request, 'storage_information.html', context)
 
